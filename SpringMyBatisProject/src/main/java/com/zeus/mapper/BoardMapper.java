97e7797bd6c6365ab308527c49676ef3707283d2
KIM SOOBIN <xpsxm225@naver.com>
2023-01-05 22:50:38 +0900

    230105_04

--- SpringMyBatisProject/src/main/java/com/zeus/mapper/BoardMapper.java
+++ SpringMyBatisProject/src/main/java/com/zeus/mapper/BoardMapper.java
@@ -17,5 +17,6 @@ public interface BoardMapper {
 
 	public List<Board> list() throws Exception;
 
+	//@Param 어노테이션을 붙이면 본인이 원하는 명으로 mapper에서 사용할 수 있다. 아래와 같은 경우는 #{title}로 사용할 수 있다.
 	public List<Board> search(@Param("title") String title) throws Exception;
 }
